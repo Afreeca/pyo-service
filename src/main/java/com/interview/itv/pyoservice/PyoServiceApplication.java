c632e7f08ea8fb0406b282ba84bda94082065c0d
Afreeca  Mendes <adilsonmendesramos@gmail.com>
2020-12-05 20:17:37 +0000

    adding project structure

---
+++ src/main/java/com/interview/itv/pyoservice/PyoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.interview.itv.pyoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/itv/pyoservice/PyoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.itv.pyoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PyoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
